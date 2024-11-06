0b014d1355b691c162d2186efbe785c66b2c18cc
vidakpop <daviskpop@gmail.com>
2024-11-06 09:48:07 +0300

    "Update App.jsx and HeroSection.jsx: fix typo in className, add React import, and modify HeroSection h1 element with gradient text"

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
      <Navbar />
-     <div className="max-w7xl mx-auto pt-20 px-6">
+     <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
      </div>
     
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,14 @@
 import video1 from '../assets/video1.mp4';
 import video2 from '../assets/video2.mp4';
+import React from 'react';
 export const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
-        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">VirtualR build tools for developers</h1>
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">VirtualR build tools
+            <span className='bg-gradient-to-r from-orange-500 to-red-800 bg-clip-text text-transparent' >
+            {""} for developers
+            </span>
+            </h1>
     </div>
   )
 }

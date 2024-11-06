3cd90bdcd39af641ceb00cd2c226c76b37a523a4
vidakpop <daviskpop@gmail.com>
2024-11-06 10:10:19 +0300

    "Added buttons and videos to HeroSection component"

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,7 +14,24 @@ export const HeroSection = () => {
 
             </p>
             <div className="flex justify-center my-10">
-                
+                <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3
+                rounded-md ">
+                  Get started for free
+                </a>
+                <a href="#"className='py-3 px-4 mx-3 border rounded-md'>
+                    Documentation
+                </a>
+
+            </div>
+            <div className='flex justify-center mt-10'>
+                <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' >
+                    <source src={video1} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' >
+                    <source src={video2} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
     </div>
   )

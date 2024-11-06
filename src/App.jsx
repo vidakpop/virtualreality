7f9838a6caa05056ea2e2c6341887be0617571c1
vidakpop <daviskpop@gmail.com>
2024-11-06 09:30:07 +0300

    "Added HeroSection component to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { HeroSection } from './components/HeroSection'
 
 const App = () => {
   return (
     <>
      <Navbar />
+     <div className="max-w7xl mx-auto pt-20 px-6">
+     <HeroSection />
+     </div>
+    
     </>
   )
 }

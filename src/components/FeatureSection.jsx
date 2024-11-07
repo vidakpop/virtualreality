d116bbdc9b2e87eaf18d4ea8ea5c3306712b95b2
vidakpop <daviskpop@gmail.com>
2024-11-07 09:29:09 +0300

    "Updated FeatureSection component to include new HTML structure and CSS classes."

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FeatureSection = () => {
   return (
-    <div>FeatureSection</div>
+    <div className='relative mt-20 border-b border-neutral-800 min-h-[800px]'>
+        <div className="text-center">
+            <span className='bg-neutral-900 text-orenge-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase' >
+                Feature
+            </span>
+        </div>
+    </div>
   )
 }
 

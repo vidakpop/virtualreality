e010c094b42a244e6a8b416ad215e5d7db813820
vidakpop <daviskpop@gmail.com>
2024-11-08 13:14:04 +0300

    "Added import statement for 'features' constant and empty div element to FeatureSection component"

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {features} from '../constants'
 const FeatureSection = () => {
   return (
     <div className='relative mt-20 border-b border-neutral-800 min-h-[800px]'>
@@ -13,6 +13,9 @@ const FeatureSection = () => {
                  {' '} your code
               </span>  
             </h2>
+        </div>
+        <div className='flex flex-wrap mt-10 lg:mt-20'>
+
         </div>
     </div>
   )

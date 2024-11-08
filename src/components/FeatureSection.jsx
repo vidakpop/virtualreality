ed5d2d16177b9961d50e999745fdad2d6b0c2aed
vidakpop <daviskpop@gmail.com>
2024-11-08 13:05:28 +0300

    "Added a new heading element to the FeatureSection component"

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -7,6 +7,12 @@ const FeatureSection = () => {
             <span className='bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase' >
                 Feature
             </span>
+            <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking wide'>
+              Easily build 
+              <span className='bg-gradient-to-r from-orange-500 to-orange-800 bg-clip-text text-transparent'>
+                 {' '} your code
+              </span>  
+            </h2>
         </div>
     </div>
   )

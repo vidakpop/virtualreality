67f8099040c5af18490abfea0b1e79f457bede43
vidakpop <daviskpop@gmail.com>
2024-11-10 07:32:32 +0300

    "Added feature icon rendering in FeatureSection component"

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -16,7 +16,11 @@ const FeatureSection = () => {
         </div>
         <div className='flex flex-wrap mt-10 lg:mt-20'>
             {features.map((feature,index)=>(
-              <div></div>
+              <div key={index} className='w-full sm:1/2 lg:w-1/3'>
+                <div className='flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full'>
+                  {feature.icon}
+                </div>
+              </div>
 
             ))}
         </div>

ba1519101c87ccc383dcb7456dd36c79cba6e16e
vidakpop <daviskpop@gmail.com>
2024-11-10 18:28:56 +0300

    "Updated Pricing component with new layout and styling"

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -3,7 +3,20 @@ import { CheckCircle2 } from 'lucide-react'
 import { pricingOptions } from '../constants'
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <div className='mt-20'>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wider'>
+            Pricing
+        </h2>
+        <div className='flex flex-wrap'>
+            {pricingOptions.map((option, index) => (
+                <div key={index} className='w-full md:w-1/2 lg:w-1/3 p-2'>
+                    <div className='p-10 border border-neutral-700 rounded-xl'>
+                        <p>{option.title}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 

77e1be6e27587510bdb00d86c01707bb317d1de8
vidakpop <daviskpop@gmail.com>
2024-11-10 08:28:12 +0300

    "Added imports and updated Workflow component to include a heading and image"

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { CheckCircle2 } from 'lucide-react'
+import codeImg from '../assets/code.jpg'
 const Workflow = () => {
   return (
-    <div>Workflow</div>
+    <div className='mt-20'>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>
+            Accelerate your coding Workflow.
+        </h2>
+    </div>
   )
 }
 

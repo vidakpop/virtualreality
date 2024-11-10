180cb86b216548a4632d38bfd6642f319fa05d71
vidakpop <daviskpop@gmail.com>
2024-11-10 08:35:57 +0300

    "Update Workflow component: add gradient text effect to 'coding Workflow' phrase in header"

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -5,7 +5,8 @@ const Workflow = () => {
   return (
     <div className='mt-20'>
         <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>
-            Accelerate your coding Workflow.
+            Accelerate your
+            <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'> coding Workflow.</span>
         </h2>
     </div>
   )

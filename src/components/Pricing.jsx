874c66a31571175dfabd901dcc2e6e507914285e
vidakpop <daviskpop@gmail.com>
2024-11-10 18:42:13 +0300

    Added price display to pricing options in Pricing component.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -19,6 +19,12 @@ const Pricing = () => {
                                 </span>
                             )}
                         </p>
+                        <p className='mb-8'>
+                            <span className='text-5xl mt-6 mr-2'>
+                                {option.price}
+                            </span>
+                            <span className='text-neutral-400 tracking-tight'>/Month</span>
+                        </p>
                     </div>
                 </div>
             ))}

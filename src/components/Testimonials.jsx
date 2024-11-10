41dd6e922b8437bb41173f27f8a553307fffbc6e
vidakpop <daviskpop@gmail.com>
2024-11-10 19:35:24 +0300

    "Added h2 element with text and styles to Testimonials component"

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,7 +4,9 @@ import { testimonials } from '../constants'
 const Testimonials = () => {
   return (
     <div className='mt-20 tracking-wide'> 
-        h2
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20'>
+            What people are saying
+        </h2>
     </div>
   )
 }

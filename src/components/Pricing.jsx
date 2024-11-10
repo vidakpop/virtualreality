4363ed5864afda335824cae40f4ebf21461f4813
vidakpop <daviskpop@gmail.com>
2024-11-10 18:36:31 +0300

    "Updated Pricing component to add a badge to the 'Pro' pricing option"

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -11,7 +11,14 @@ const Pricing = () => {
             {pricingOptions.map((option, index) => (
                 <div key={index} className='w-full md:w-1/2 lg:w-1/3 p-2'>
                     <div className='p-10 border border-neutral-700 rounded-xl'>
-                        <p>{option.title}</p>
+                        <p className='text-4xl mb-8'>
+                            {option.title}
+                            {option.title==='Pro' && (
+                                <span className='bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2'>
+                                    (Most Popular)
+                                </span>
+                            )}
+                        </p>
                     </div>
                 </div>
             ))}

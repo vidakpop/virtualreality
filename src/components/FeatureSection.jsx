2f1f8e0e9f6249274c691eb4fb3e94d8210890a3
vidakpop <daviskpop@gmail.com>
2024-11-10 07:48:35 +0300

    "Added class to paragraph element in FeatureSection component"

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -5,7 +5,7 @@ const FeatureSection = () => {
     <div className='relative mt-20 border-b border-neutral-800 min-h-[800px]'>
         <div className="text-center">
             <span className='bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase' >
-                Feature
+                Features
             </span>
             <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking wide'>
               Easily build 
@@ -23,7 +23,7 @@ const FeatureSection = () => {
                 </div>
                 <div>
                   <h5 className='mt-1 mb-6 text-xl'>{feature.text}</h5>
-                  <p>{feature.description}</p>
+                  <p className='text-md p-2 mb-20 text-neutral-500'>{feature.description}</p>
                 </div>
               </div>
               </div>

f50ba1a305132798ce0029396838928f1ce825dc
vidakpop <daviskpop@gmail.com>
2024-11-10 08:51:59 +0300

    "Added checklist items component to Workflow.jsx"

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CheckCircle2 } from 'lucide-react'
 import codeImg from '../assets/code.jpg'
+import { checklistItems } from '../constants'
 const Workflow = () => {
   return (
     <div className='mt-20'>
@@ -11,6 +12,15 @@ const Workflow = () => {
         <div className='flex flex-wrap justify-center'>
             <div className='p-2 w-full lg:w-1/2'>
                 <img src={codeImg} alt="code" />
+            </div>
+            <div className='pt-12 w-full lg:w-1/2'>
+                {checklistItems.map((item,index)=>(
+                    <div key={index} className='flex mb-12'>
+                        <div className='text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full'>
+                            <CheckCircle2/>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     </div>

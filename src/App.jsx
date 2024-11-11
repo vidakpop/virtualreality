686af2c08e90c4794bee4f02756e47c293d8df44
vidakpop <daviskpop@gmail.com>
2024-11-11 06:49:46 +0300

    "Added Footer component import and rendered it in App component"

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FeatureSection from './components/FeatureSection'
 import Workflow from './components/Workflow'
 import Pricing from './components/Pricing'
 import Testimonials from './components/Testimonials'
-
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
@@ -16,6 +16,7 @@ const App = () => {
         <Workflow />
         <Pricing />
         <Testimonials />
+        <Footer />
      </div>
     
     </>

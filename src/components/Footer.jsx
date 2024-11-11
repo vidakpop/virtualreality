cb34bdbf17f3e14742afa16addd2b00687031812
vidakpop <daviskpop@gmail.com>
2024-11-11 07:06:42 +0300

    "Added new section to Footer component with 'Platform' heading and list of links"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,6 +15,16 @@ const Footer = () => {
                     ))}
                 </ul>
             </div>
+            <div>
+                <h3 className='text-md font-semibold mb-4'>Platform</h3>
+                <ul className='space-y-2'>
+                    {platformLinks.map((link,index)=>(
+                        <li key={index}>
+                          <a className='text-neutral-300 hover:text-white' href={link.href}>{link.text}</a>  
+                        </li>
+                    ))}
+                </ul>
+            </div>
             
         </div>
     </footer>

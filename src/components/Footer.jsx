a60cbabccdb93495735391000e0980a29e1006b2
vidakpop <daviskpop@gmail.com>
2024-11-11 07:08:20 +0300

    "Added Community section with links to Footer component"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,6 +25,16 @@ const Footer = () => {
                     ))}
                 </ul>
             </div>
+            <div>
+                <h3 className='text-md font-semibold mb-4'>Community</h3>
+                <ul className='space-y-2'>
+                    {communityLinks.map((link,index)=>(
+                        <li key={index}>
+                          <a className='text-neutral-300 hover:text-white' href={link.href}>{link.text}</a>  
+                        </li>
+                    ))}
+                </ul>
+            </div>
             
         </div>
     </footer>
